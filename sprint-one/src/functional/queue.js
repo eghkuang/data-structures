fb319c0ee7e515fe9a30aed0490509d1596c1480
Esther Kuang <eghkuang@gmail.com>
2022-04-13 16:05:57 -0700

    functional pattern complete

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,36 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var enqueue = 0;
+  var dequeue = 0;
+
+  //FIFO - first element added to queue = first one to be removed
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    // Add a string to the back of the queue
+    storage[enqueue] = value;
+    enqueue++;
   };
 
   someInstance.dequeue = function() {
+    // Remove and return the string at the front of the queue
+    // debugger;
+
+    if (enqueue - dequeue) {
+      // debugger;
+      var item = storage[dequeue];
+      delete storage[dequeue];
+      dequeue++;
+      return item;
+    }
   };
 
   someInstance.size = function() {
+    // Return the number of items in the queue
+    return enqueue - dequeue;
+
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,16 +3,26 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var numericKey = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[numericKey] = value;
+    numericKey++;
   };
 
   someInstance.pop = function() {
+    if (numericKey > 0) {
+      numericKey--;
+    }
+    return storage[numericKey];
+    delete storage[numericKey];
   };
 
   someInstance.size = function() {
+    return numericKey;
   };
 
   return someInstance;
 };
+
