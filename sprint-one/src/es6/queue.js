226c8c2ac1417dc892ed6f7999d0c6741567a735
Esther Kuang <eghkuang@gmail.com>
2022-04-14 15:48:07 -0700

    part one complete

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -1,7 +1,34 @@
 class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  // Capitalize your function name to indicate to others that it is intended to be run with the keyword new
+  // Use the keyword new when instantiating your class
+  // Use the keyword this in your constructor
+  // Explicitly declare a class method named constructor
+  // Declare all other class methods within the class declaration
+
   constructor() {
+    this.storage = {};
+    this.end = 0;
+    this.start = 0;
   }
 
+  enqueue(value) {
+    this.storage[this.end] = value;
+    this.end++;
+  }
+
+  dequeue() {
+    if (this.end > this.start) {
+      var item = this.storage[this.start];
+      delete this.storage[this.start];
+      this.start++;
+      return item;
+    }
+  }
+
+  size() {
+    return this.end - this.start;
+  }
 }
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -1,7 +1,33 @@
 class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  // Capitalize your function name to indicate to others that it is intended to be run with the keyword new
+  // Use the keyword new when instantiating your class
+  // Use the keyword this in your constructor
+  // Explicitly declare a class method named constructor
+  // Declare all other class methods within the class declaration
+
   constructor() {
+    this.storage = {};
+    this.numericKey = 0;
+  }
+
+  push(value) {
+    this.storage[this.numericKey] = value;
+    this.numericKey++;
+  }
+
+  pop() {
+    if (this.numericKey > 0) {
+      this.numericKey--;
+    }
+    return this.storage[this.numericKey];
+    delete this.storage[this.numericKey];
+  }
+
+  size() {
+    return this.numericKey;
   }
 
 }
