2c3a6b6557e99f39802cc274f46cefb0acb94040
Esther Kuang <eghkuang@gmail.com>
2022-04-14 14:13:22 -0700

    current progress

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,32 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  //Capitalize your function name to indicate to others that it is intended to be run with the keyword new
+  // Use the keyword new when instantiating your class
+  // Use the keyword this in your constructor
+
+  //end = enqueue, start = dequeue;
+  this.storage = {};
+  this.end = 0;
+  this.start = 0;
 };
 
+Queue.prototype.enqueue = function(value) {
+  this.storage[this.end] = value;
+  this.end++;
+};
+
+Queue.prototype.dequeue = function() {
+  if (this.end > this.start) {
+    var item = this.storage[this.start];
+    delete this.storage[this.start];
+    this.start++;
+    return item;
+  }
+};
+
+Queue.prototype.size = function() {
+  return (this.end - this.start);
+};
 
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,28 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  //Capitalize your function name to indicate to others that it is intended to be run with the keyword new
+  // Use the keyword new when instantiating your class
+  // Use the keyword this in your constructor
+  this.storage = {};
+  this.numericKey = 0;
 };
 
+Stack.prototype.push = function(value) {
+  this.storage[this.numericKey] = value;
+  this.numericKey++;
+};
+
+Stack.prototype.pop = function() {
+  if (this.numericKey > 0) {
+    this.numericKey--;
+  }
+  return this.storage[this.numericKey];
+  delete this.storage[this.numericKey];
+};
+
+Stack.prototype.size = function() {
+  return this.numericKey;
+};
 
