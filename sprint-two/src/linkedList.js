61944b8cad320aa87321d6ae4e2370474cb5129b
Esther Kuang <eghkuang@gmail.com>
2022-04-15 15:12:41 -0700

    current progress: linkedlist and tree complete

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,50 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    // iterate through the linked list if the
+    // next step is null then we add current point
+    var newNode = new Node(value);
+
+    //if statement to check if list.head is valid
+    if (list.head === null) {
+      list.head = newNode;
+      list.tail = newNode;
+    } else {
+      var last = list.head;
+      while (last.next !== null) {
+        last = last.next;
+      }
+      last.next = newNode;
+      list.tail = newNode;
+    }
+    //iterate through nodes
+
+
   };
 
   list.removeHead = function() {
+    // if this.head is true remove and assign next to node
+    if (!list.head) {
+      return;
+    }
+    var oldHead = list.head.value;
+    list.head = list.head.next;
+    return oldHead;
   };
 
   list.contains = function(target) {
+    // while loop
+    if (!list.head) {
+      return;
+    }
+    var first = list.head;
+    while (first !== null) {
+      if (first.value === target) {
+        return true;
+      }
+      first = first.next;
+    }
+    return false;
   };
 
   return list;
@@ -24,6 +62,14 @@ var Node = function(value) {
   return node;
 };
 
+
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+// .head property, a linkedListNode instance
+// .tail property, a linkedListNode instance
+// .addToTail() method, takes a value and adds it to the end of the list
+// .removeHead() method, removes the first node from the list and returns its value
+// .contains() method, returns boolean reflecting whether or not the passed-in value is in the linked list
+// What is the time complexity of the above functions?
--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,7 +3,9 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];
+
+  _.extend(newTree, treeMethods);
 
   return newTree;
 };
@@ -11,13 +13,37 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  // push value to children
+  // if array has a value add a nested array inside with another value
+  var result = new Tree(value);
+  this.children.push(result);
 };
 
 treeMethods.contains = function(target) {
+
+  if (this.value === target) {
+    return true;
+  }
+
+  for (var i = 0; i < this.children.length; i++) {
+    if (this.children[i].contains(target)) {
+      return true;
+    }
+  }
+  return false;
 };
 
+
+// A tree class, in functional with shared methods style, with the following properties:
+
+//  .children property, an array containing a number of subtrees
+
+//  .addChild() method, takes any value, sets that as the target of a node, and adds that node as a child of the tree
+
+//  A .contains() method, takes any input and returns a boolean reflecting whether it can be found as the value of the target node or any descendant node
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
